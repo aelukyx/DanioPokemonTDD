43e70ccd5b5faef50bf3f4f207bd38cac6175c62
Alexander Raúl Cabrera Guevara <arcabrerag@outlook.com>
2014-11-15 10:18:09 -0500

    MasterPokemon.Vs.1.0
    Listo y probado en tiempo de ocio. :ghost:

---
+++ LabPokemon/CalcPokeTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using MasterPokemon;
+
+namespace LabPokemon
+{
+    [TestFixture]
+    public class CalcPokeTest
+    {
+
+        private PokemonBattle pokemonBattle { get; set; }
+
+        [SetUp]
+        public void InstaciasAntesDeCadaTest()
+        {
+            // Arrange
+            pokemonBattle = new PokemonBattle();
+        }
+
+        [Test]
+        public void TestBattleAguaElec8y4()
+        {
+            // Act
+            var result = pokemonBattle.CalcularDanho("AGUA", "ELECTRICO", 8, 4);
+
+            // Assert
+            Assert.AreEqual(100, Convert.ToInt32(result));
+        }
+
+        [Test]
+        public void TestBattleAguaHier15y20()
+        {
+            // Act
+            var result = pokemonBattle.CalcularDanho("AGUA", "HIERBA", 15, 20);
+
+            // Assert
+            Assert.AreEqual(19, Convert.ToInt32(result));
+        }
+
+        [Test]
+        public void TestBattleFuegFueg17y13()
+        {
+            // Act
+            var result = pokemonBattle.CalcularDanho("FUEGO", "FUEGO", 17, 13);
+
+            // Assert
+            Assert.AreEqual(33, Convert.ToInt32(result));
+        }
+
+
+
+    }
+}
---
+++ MasterPokemon/PokemonBattle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterPokemon
+{
+    public class PokemonBattle
+    {
+        double damageTotal;
+
+        public double CalcularDanho(string tipo1, string tipo2, double attack, double defense)
+        {
+            var efficacy = new Dictionary<string, double>();
+
+            efficacy.Add("FUEGO.FUEGO", 0.5); efficacy.Add("FUEGO.HIERBA", 2); efficacy.Add("FUEGO.ELECTRICO", 1); efficacy.Add("FUEGO.AGUA", 0.5);
+            efficacy.Add("HIERBA.FUEGO", 0.5); efficacy.Add("HIERBA.HIERBA", 0.5); efficacy.Add("HIERBA.ELECTRICO", 1); efficacy.Add("HIERBA.AGUA", 2);
+            efficacy.Add("ELECTRICO.FUEGO", 1); efficacy.Add("ELECTRICO.HIERBA", 0.5); efficacy.Add("ELECTRICO.ELECTRICO", 0.5); efficacy.Add("ELECTRICO.AGUA", 2);
+            efficacy.Add("AGUA.FUEGO", 2); efficacy.Add("AGUA.HIERBA", 0.5); efficacy.Add("AGUA.ELECTRICO", 1); efficacy.Add("AGUA.AGUA", 0.5);
+            
+            damageTotal = 50 * (attack / defense) * efficacy[(tipo1+"."+tipo2)];
+
+            return  damageTotal;
+
+        }
+    }
+}
